4b66f3b641ec649dcef8edc65514fd468dfac5b1
munyite-megzs <margaret.sitati1@gmail.com>
2020-01-27 21:02:15 +0300

    modified thecomments in the pair function

--- Pairs/Program.cs
+++ Pairs/Program.cs
@@ -21,19 +21,19 @@ namespace Pairs
 
             //Sort the array first 
             Array.Sort(arr);
-            //this will hold the count varible
+            //this varible will hold the count to the number of success results
             int count = 0;
             //we will use the pointer technique to solve this challange;
             //have two pointers
             //the first pointer represents the first elment in the array
             int i = 0;
-            //the second pointer represents the last element in the array
+            //the second pointer represents the second element in the array
             int j = 1;
             int n = arr.Length;
 
             while (j < n)
             {
-                //if we find a pair, increment the results
+                //if we find a pair, increment the count
                 if (arr[j] - arr[i] == k)
                 {
                     count++;
