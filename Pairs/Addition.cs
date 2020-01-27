a7975c8316e9034bc0265622a6777b579000421a
munyite-megzs <margaret.sitati1@gmail.com>
2020-01-27 18:06:28 +0300

    modified the pairs function

---
+++ Pairs/Addition.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pairs
+{
+    class Addition
+    {
+        static bool isPairSum(int[] A, int N, int X)
+        {
+            // represents first pointer 
+            int i = 0;
+
+            // represents second pointer 
+            int j = N - 1;
+
+            while (i < j)
+            {
+
+                // If we find a pair 
+                if (A[i] + A[j] == X)
+                    return true;
+
+                // If sum of elements at current 
+                // pointers is less, we move towards 
+                // higher values by doing i++ 
+                else if (A[i] + A[j] < X)
+                    i++;
+
+                // If sum of elements at current 
+                // pointers is more, we move towards 
+                // lower values by doing j-- 
+                else
+                    j--;
+            }
+            return false;
+        }
+    }
+}
